8a59cb1c9e2682b36ee3cfde7706a56437e57aec
RATED-R-SUNDRAM <ss6437p@gmail.com>
2024-06-18 23:22:45 +0530

    PYSPARK NOT INITALIZING

---
+++ code.py
@@ -0,0 +1,18 @@
+#%%
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("shivam").getOrCreate()
+
+#%%
+
+data = [
+    ("A",10,"pat"),
+    ("B",20,"cat"),
+    ("C",30,"dat"),
+    ("D",40,"eat"),
+]
+
+schemma =['NAME','AGE','HOBBY']
+df =spark.DataFrame(data,schemma)
+# %%
